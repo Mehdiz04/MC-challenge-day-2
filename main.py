2adc29d8fc63ac76d9b10ddfbd4ddb17e0ca5ce4
Mehdi Zerouak <118940145+Mehdiz04@users.noreply.github.com>
2023-04-17 23:46:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def romanToInt(s):
 
 def intToRoman(num):
     SymValues = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D',900: 'CM', 1000: 'M'}
-    numbers = sorted(SymValues.keys() , reverse = True)
+    numbers = reversed(SymValues.keys())
     roman = ''
     for n in numbers:
         while n <= num:
